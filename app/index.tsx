34b86c61514a5a50bbcb6a859543afa7115ada4c
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-20 13:57:16 +0300

    ana ekran oluşturma

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+function Index() {
+  return (
+    <View>
+      <Text style={{
+        fontSize: 24
+      }}>Hello World!</Text>
+    </View>
+  );
+}
+
+export default Index;
