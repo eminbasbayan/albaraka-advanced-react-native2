bed622cdd6f887369566385b07d3613b40d9d6ec
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-21 14:59:59 +0300

    Truthy ve Falsy

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -3,17 +3,28 @@ import React, { useContext, useState } from 'react';
 import { CartContext } from '@/context/CartContext';
 import ProductCard from '@/components/products/ProductCard';
 
+const CartItems = ({ cartItems, toplamDeger }) => {
+  return (
+    <>
+      {cartItems?.map((item) => (
+        <ProductCard key={item.id} item={item} cart />
+      ))}
+
+      {cartItems.length && <Text>Sepet Toplam: ₺{toplamDeger}</Text>}
+    </>
+  );
+};
+
 const CartScreen = () => {
   const { cartItems } = useContext(CartContext);
-
-  
+  const toplamDeger = cartItems?.reduce((toplam, urun) => {
+    return toplam + urun.price * urun.quantity;
+  }, 0);
 
   return (
     <View>
       <Text>CartScreen</Text>
-      {cartItems.map((item) => (
-        <ProductCard key={item.id} item={item} cart />
-      ))}
+      <CartItems cartItems={cartItems} toplamDeger={toplamDeger} />
     </View>
   );
 };
